f2a7a2ce2edbdff3cba3cd36d783eedfcb03ae82
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-02-08 06:15:43 -0800

    Initial commit

--- index.js
+++ index.js
@@ -31,6 +31,18 @@ const url = require('url');
 
 ////////////////////////
 //// SERVER
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product, productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.nutrients);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+};
+
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   'utf-8'
@@ -52,6 +64,9 @@ const server = http.createServer((req, res) => {
   // OVERVIEW page
   if (pathName === '/' || pathName === '/overview') {
     res.writeHead(200, { 'Content-type': 'text/html' });
+
+    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el));
+
     res.end(tempOverview);
 
     // PRODUCT page
