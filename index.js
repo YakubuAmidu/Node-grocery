1e876674f09f9752a62b257a4110392e63fd78c2
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-02-09 01:10:24 -0800

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
 //////////////////////////
-/// FILES
+//// FILES
 
 // // Blocking synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -46,6 +47,9 @@ const tempProduct = fs.readFileSync(
 );
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
